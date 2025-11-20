ccf63d08202ce31f59fbd8f2d4139174c6573ebe
luut189 <73666044+luut189@users.noreply.github.com>
2025-11-20 17:18:20 -0500

    refactor: simplify resume download handler to use Response object

--- src/app/api/resume/route.ts
+++ src/app/api/resume/route.ts
@@ -1,12 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
   const response = await fetch(
     'https://raw.githubusercontent.com/luut189/latex-repo/gh-pages/resume.pdf',
   );
   const buffer = await response.arrayBuffer();
 
-  res.setHeader('Content-Disposition', 'attachment; filename=TuongLuuResume.pdf');
-  res.setHeader('Content-Type', 'application/pdf');
-  res.send(Buffer.from(buffer));
+  return new Response(buffer, {
+    headers: {
+      'Content-Disposition': 'attachment; filename=TuongLuuResume.pdf',
+      'Content-Type': 'application/pdf',
+    },
+  });
 }
